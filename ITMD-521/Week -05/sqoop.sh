e9a197212c276fd765854a738a306bb8db735e1a
sadaasree <35338353+sadaasree@users.noreply.github.com>
2018-02-15 22:30:33 -0600

    Delete sqoop.sh

--- ITMD-521/Week -05/sqoop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-clear
-echo "CREATE TABLE temperature (id INT NOT NULL AUTO_INCREMENT,year YEAR NOT NULL,temperature INT,PRIMARY KEY(id));"
-echo "INSERT INTO temperature SELECT * FROM records WHERE temperature >= 38;"
-echo "sqoop import --connect jdbc:mysql://localhost/521 --table temperature -m 1 --username root -P"
-echo "hadoop fs -tail temperature/part-m-00000"
