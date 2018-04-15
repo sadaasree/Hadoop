9474de802bd5d911799342ebcc4bec6358b3647e
sadaasree <35338353+sadaasree@users.noreply.github.com>
2018-04-15 18:49:43 -0500

    Create spark.py

---
+++ ITMD-521/Week-13/spark.py
@@ -0,0 +1,23 @@
+
+f = open('spark1950.txt', 'w')
+with open('1950.txt') as temp:
+
+     for record in temp:
+
+         text = record.strip()
+
+         if record[87] == '+' :
+
+            (longitude,temperature,q) = (text[35:41], text[88:92], text[92:93])
+            longitude=int(longitude)/1000
+            longitude=str(longitude)
+            f.write(longitude + "\t" + temperature +"\t"+q +"\n")
+
+
+         else :
+
+            (longitude,temperature,q) = (text[35:41], text[87:92], text[92:93])
+            longitude=int(longitude)/1000
+            longitude=str(longitude)
+            f.write(longitude + "\t" + temperature +"\t"+ q +"\n")
+f.close()
