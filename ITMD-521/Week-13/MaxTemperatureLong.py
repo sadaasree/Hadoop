511e1a07b553f6001f3c6fa0d79463348139694c
sadaasree <35338353+sadaasree@users.noreply.github.com>
2018-04-19 17:06:06 -0500

    Update MaxTemperatureLong.py

--- ITMD-521/Week-13/MaxTemperatureLong.py
+++ ITMD-521/Week-13/MaxTemperatureLong.py
@@ -8,35 +8,21 @@ sc = SparkContext("local", "Max Temperature")
 
 sqlContext = SQLContext(sc)
 
-f1=open("aggregation.txt","w")
+f1=open("results.txt","w")
 
 lines = sc.textFile(sys.argv[1]) \
-
-  .map(lambda s: s.split("\t")) \
-
-  .filter(lambda rec: (int(rec[1]) != 9999 and re.match("[01459]", rec[2]))) \
-
-  .map(lambda rec: (int(rec[0]), int(rec[1]))) \
-
-  .sortByKey(ascending=True)
-
-minlong =-180
-
+ .map(lambda s: s.split("\t")) \
+ .filter(lambda rec: (int(rec[1]) != 9999 and re.match("[01459]", rec[2]))) \
+ .map(lambda rec: (int(rec[0]), int(rec[1]))) \
+ .sortByKey(ascending=True)
+longitude =-180
 print ("Calculating max temperature for longitude range 10...")
-
-while(minlong<180):
-
-    filtered = lines.filter(lambda rec: (int(rec[0])>=minlong and int(rec[0])<=minlong+10)).reduceByKey(max)
-
-    df = sqlContext.createDataFram e(filtered, ['longitude', 'temp'])
-
+while(longitude<180):
+    filtered = lines.filter(lambda rec: (int(rec[0])>=longitude and int(rec[0])<=longitude+10)).reduceByKey(max)
+    df = sqlContext.createDataFrame(filtered, ['longitude', 'temp'])
     output = df.agg({"temp": "max"})
-
-    print (minlong,minlong+10,output.collect())
-
+    print (longitude,longitude+10,output.collect())
     maxtemp=output.collect()
-
-    f1.write(str(minlong)+" to "+str(minlong+10)+" = "+str(output.collect())+"\n")
-
-    minlong=minlong+10
+    f1.write(str(longitude)+" to "+str(longitude+10)+" = "+str(output.collect())+"\n")
+    longitude=longitude+10
 
